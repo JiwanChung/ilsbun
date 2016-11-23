88c824970629488c20a2cd894df712c3b4d2f0d2
unknown <>
2016-11-24 04:08:55 +0900

    plz

--- web.js
+++ web.js
@@ -1,7 +1,3 @@
-process.env.MONGO_URL = 'mongodb://jiwan:beetle27@ds031597.mlab.com:31597/ils';
-process.env.PORT = 8001;
-console.log(process.env.PORT);
-process.env.ROOT_URL = 'http://localhost:8001';
 process.argv.splice(2, 0, 'program.json');
 process.chdir(require('path').join(__dirname, 'programs', 'server'));
 require('./boot.js');
