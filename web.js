cc35d068ffd39fadcfae2e10d4b96bc69f987cda
unknown <>
2016-11-25 00:49:21 +0900

    config

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 process.env.MONGO_URL = 'mongodb://jiwan:beetle27@ds031597.mlab.com:31597/ils';
 process.env.PORT = 8002;
 console.log(process.env.PORT);
-console.log("WTF");
 process.env.ROOT_URL = 'http://localhost:8002';
-process.argv.splice(2, 0, './program.json');
-process.chdir(require('path').join(__dirname, 'yslaw2_ystemp'));
+console.log(__dirname);
+process.argv.splice(2, 0, 'program.json');
+process.chdir(__dirname);
 require('./boot.js');
