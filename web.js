3727f1a71fccf02240cb91f55c105bb9951a7fa0
unknown <>
2016-11-24 23:36:12 +0900

    dsd

--- web.js
+++ web.js
@@ -4,5 +4,4 @@ console.log(process.env.PORT);
 console.log("WTF");
 process.env.ROOT_URL = 'http://ystemp.cafe24app.com:8002';
 process.argv.splice(2, 0, 'program.json');
-process.chdir(require('path'));
 require('./boot.js');
