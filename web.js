6bdb09cd7a264e6b8c4edf1443ecc962c6d42c17
unknown <>
2016-11-25 00:05:12 +0900

    config

--- web.js
+++ web.js
@@ -4,4 +4,5 @@ console.log(process.env.PORT);
 console.log("WTF");
 process.env.ROOT_URL = 'http://localhost:8002';
 process.argv.splice(2, 0, 'program.json');
+process.chdir(require('path').dirname(__dirname));
 require('./boot.js');
